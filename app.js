6c746a056e8b30f7e6424034614843a9fecbd794
MM Mohi <mohiuddin0240@gmail.com>
2021-03-26 23:15:26 +0600

    song search and find the lyrics

---
+++ app.js
@@ -0,0 +1,58 @@
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchText = document.getElementById("input-search").value;
+  const url = ` https://api.lyrics.ovh/suggest/${searchText}`;
+
+  try {
+    const fetchSearchText = async () => {
+      const response = await fetch(url);
+      const { data } = await response.json();
+      displaySongDetails(data);
+    };
+
+    fetchSearchText();
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+const displaySongDetails = songs => {
+  let html = "";
+  songs.forEach(
+    song =>
+      (html += `
+      <div class="single-result row align-items-center my-3 p-3">
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+            <audio controls>
+              <source src=${song.preview} type="audio/ogg">
+            </audio>
+        </div>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyrics('${song.artist.name}', '${song.title}')" lyrics-btn class="btn btn-success">Get Lyrics</button>
+        </div>
+      </div>
+    `)
+  );
+  document.getElementById("song-wrapper").innerHTML = html;
+};
+
+const getLyrics = (artist, title) => {
+  try {
+    const fetchLyrics = async () => {
+      const response = await fetch(
+        `https://api.lyrics.ovh/v1/${artist}/${title}`
+      );
+      const data = await response.json();
+      displayLyrics(data.lyrics);
+    };
+
+    fetchLyrics();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const displayLyrics = lyrics => {
+  document.getElementById("lyrics-song").innerText = lyrics;
+};
